88a6658f35c042b6dc4effddf2715b83ba628c12
njmalaza <junior.malaza@rysis.co.za>
2022-07-06 12:19:12 +0200

    first commit

---
+++ js/index.vanta.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.HALO({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+  });
+});
